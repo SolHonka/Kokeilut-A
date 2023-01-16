8a7b32a8a23ef37e0a87ea37fba7d2bf49e60f78
Sol <salomonhonk@gmail.com>
2023-01-16 13:35:53 +0200

    Moduuli 3 - Palautus

---
+++ Ohjelmointitehtavat/Module3.py
@@ -0,0 +1,54 @@
+#1
+
+KalanPituus = float(input("Mikä on kalan pituus?"))
+
+if KalanPituus <= 37:
+    print("Kala on alamittainen!", 37-KalanPituus, "cm liian pieni!")
+
+
+#2
+
+Luokat = {
+    "LUX": "on parvekkeellinen hytti yläkannella.",
+    "A": "on ikkunallinen hytti autokannen yläpuolella.",
+    "B": "on ikkunaton hytti autokannen yläpuolella.",
+    "C": "on ikkunaton hytti autokannen alapuolella."
+}
+Luokka = str.upper(input("Anna laivan hyttiluokka:"))
+
+if not Luokat[Luokka]:
+    print(Luokka, "on virheellinne luokka!")
+else:
+    print(Luokka, Luokat[Luokka])
+
+#3
+
+Sukupuoli = str.lower(input("Anna henkilön sukupuoli:"))
+Hemoglobiini = float(input("Anna henkilön hemoglobiiniarvo:"))
+
+Hemoglobiiniarvot = {
+    "mies":range(134,195),
+    "nainen":range(117,175)
+}
+
+if Hemoglobiiniarvot[Sukupuoli]:
+    ArvoPrint = "Henkilön hemoglobiini arvo on"
+
+    if Hemoglobiini in Hemoglobiiniarvot[Sukupuoli]:
+        print(ArvoPrint, "normaali.")
+    elif Hemoglobiini > max(Hemoglobiiniarvot[Sukupuoli]):
+        print(ArvoPrint, "liiaan korkea!")
+    elif Hemoglobiini < min(Hemoglobiiniarvot[Sukupuoli]):
+        print(ArvoPrint, "liian alhainen!")
+else:
+    print("Virheellinen sukupuoli!")
+
+
+#4
+
+Vuosi = int(input("Anna vuosi:"))
+
+if Vuosi % 4 == 0 and (Vuosi % 100 != 0 or Vuosi % 400 == 0):
+    print(Vuosi, "on karkausvuosi!")
+else:
+    print(Vuosi, "ei ole karkausvuosi!")
